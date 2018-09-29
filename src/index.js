a024b424a8b4162024f4ddc3d34be8bbc22d4304
mentos2805 <mentos28059@gmail.com>
2018-09-30 00:17:19 +0300

    done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,33 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    var closedCorresp = {};
+    var similar = {} 
+    for (var i=0;i<bracketsConfig.length;i++) {
+        var openBracket = bracketsConfig[i][0];
+        var closedBracket = bracketsConfig[i][1];
+        if (openBracket == closedBracket) {
+            similar[openBracket] = true;
+        } else {
+            closedCorresp[closedBracket] = openBracket;
+        }
+    }
+    var st = [];
+    for (var i=0; i<str.length;i++) {
+        var symbol = str[i];
+        var stackHead = st[st.length-1];
+ 
+        if (similar[symbol]) {
+            if (stackHead == symbol) {
+                st.pop();
+            } else {
+                st.push(symbol);
+            }
+        } else if (!closedCorresp[symbol]) {
+            st.push(symbol);
+        } else {
+            if (st.pop() != closedCorresp[symbol]) {
+                return false;
+            }
+        }
+    }
+    return (st.length == 0);
 }
